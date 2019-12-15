ad627f20d344b236f7cc3d1866ac94512dd14699
confident-token-1019 <theoonetj@gmail.com>
2019-12-15 03:23:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,4 +1,4 @@
-array = ["lil' bub", "grumpy cat", "Maru"]
+array = [1,2,15,21,23,5,8,9,7]
 element = "john"
 
 def using_include(array, element)
@@ -7,21 +7,26 @@ array.include?(element)
 end
 
 def using_sort(array)
+  array.sort
 
 end
 
 def using_reverse(array)
+  array.reverse
 
 end
 
 def using_first(array)
+  array.first
 
 end
 
 def using_last(array)
+  array.last
 
 end
 
 def using_size(array)
+  array.size
 
 end
